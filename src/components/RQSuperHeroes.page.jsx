2baa974071881f10f470a00f875a5842c10dd782
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-12-24 14:45:07 -0800

    Add query config props

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -6,18 +6,43 @@ const fetchSuperHeroes = () => {
 }
 
 export const RQSuperHeroesPage = () => {
+    const onSuccess = (data) => {
+        console.log('SUCCESS: Perform side effect after data fetching', data)
+    }
 
-    const {isLoading, data, isError, error} = useQuery('super-heroes', fetchSuperHeroes)
+    const onError = (error) => {
+        console.log('ERROR: Perform side effect after encountering error', error)
+    }
 
-    console.log(isLoading, data)
+    const {isLoading, data, isError, error, isFetching, refetch} = useQuery(
+        'super-heroes', 
+        fetchSuperHeroes,
+        {
+            cacheTime: 300000,   // Cached query results will be invalidated after 5 min (300,000ms) - default: 5 min
+            staleTime: 30000,   // 30 sec (0 by default), no fetching will be done, since last query
+            refetchOnMount: true, // also possible options: false or 'always'
+            refetchOnWindowFocus: true,
+            refetchInterval: 10000,  // continuos refetching each 10 sec, only when window is in focus
+            // refetchIntervalInBackground: 7000,
+            // enabled: false,     // use it, if you want query not be fetched automatically, but e.g. by button press only
+            onSuccess: onSuccess,   // Add success callback
+            onError: onError,   // Add error callback
+        }
+        )
+
+    console.log({ isLoading, isFetching })
 
     return (
         <div>
-            {isLoading && <h2>Data is Loading ...</h2>}
+            <h2>RQSuperHeroes Page</h2>
+
+            <button onClick={refetch}>Fetch Heroes</button>
+            
+            {(isLoading || isFetching) && <h2>Data is Loading ...</h2>}
 
             {isError && <h2>Fetching Error: {error.message}</h2>}
 
-            <h2>RQSuperHeroes Page</h2>
+            
             {data?.data.map(hero => (
                 <div key={hero.name}>{hero.name}</div>
             ))}
