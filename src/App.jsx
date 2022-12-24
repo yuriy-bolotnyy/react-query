312c73e75d5737b275c420b147efc9bfd02b9fcc
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-12-23 16:27:28 -0800

    Add links to pages

--- src/App.jsx
+++ src/App.jsx
@@ -16,28 +16,43 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <Router>
+
+      <div className="App">
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/super-heroes'>Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+
+          
+
+        </div>
+
+
+
+
+
+
+
+
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+
+    </Router>
+
   )
 }
 
