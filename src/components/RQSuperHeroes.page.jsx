31493703d63c0cad02d0958165ba743ccbdf83f8
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-12-23 21:55:51 -0800

    Add error handling

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -1,17 +1,21 @@
 import { useQuery } from "react-query"
 import axios from 'axios'
 
+const fetchSuperHeroes = () => {
+    return axios.get('http://localhost:3000/superheroes3')
+}
+
 export const RQSuperHeroesPage = () => {
 
-    const {isLoading, data} = useQuery('super-heroes', () => {
-        return axios.get('http://localhost:3000/superheroes')
-    })
+    const {isLoading, data, isError, error} = useQuery('super-heroes', fetchSuperHeroes)
 
     console.log(isLoading, data)
 
     return (
         <div>
             {isLoading && <h2>Data is Loading ...</h2>}
+
+            {isError && <h2>Fetching Error: {error.message}</h2>}
 
             <h2>RQSuperHeroes Page</h2>
             {data?.data.map(hero => (
