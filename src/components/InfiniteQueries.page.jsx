d36fbac3ac7a22a009d99239b7a9a736676af74b
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-12-25 21:09:30 -0800

    Add Infinite Queries

--- src/components/InfiniteQueries.page.jsx
+++ src/components/InfiniteQueries.page.jsx
@@ -1,31 +1,44 @@
-import { useState } from "react";
-import { useQuery } from "react-query";
+import { Fragment, useState } from "react";
+import { useInfiniteQuery } from "react-query";
 import axios from "axios";
 
 const ItemsPerPage = 2      // Desired number of item per page request e.g. 2 will return only two elements per page
 
-const fetchColors = (pageNumber) => axios.get(`http://localhost:3000/colors`)
+const fetchColors = ({ pageParam = 1 }) => axios.get(`http://localhost:3000/colors?_limit=2&_page=${pageParam}`)
 
 export const InfiniteQueriesPage = () => {
-    const [pageNumber, setPageNumber] = useState(1)
-    const { data : colors } = useQuery(
-        ['colors', pageNumber], 
-        () => fetchColors(pageNumber),
+    // const [pageNumber, setPageNumber] = useState(1)
+    const { data, hasNextPage, fetchNextPage } = useInfiniteQuery(['colors'], fetchColors,
         {
-            keepPreviousData: true,     // Keep previous data for retrieved pages
+            getNextPageParam: (_lastPage, pages) => {
+                if (pages.length <4) {  // 4 pages max <- 8 colors by 2 colors per page
+                    return pages.length + 1
+                } else {
+                    return undefined
+                }
+            },
         })
     
-    console.log('colors', colors?.data) 
+    console.log('infinite colors: ', data?.pages) 
 
     return (
         <div>
             <h2>InfiniteQueriesPage</h2>
 
-            {colors?.data?.map(color => (
-                <div key={color.id}>{color.id} - {color.label}</div>
-            ))}
+            {data?.pages.map((group, i) => {
+                return (
+                    <Fragment key={i}>
+                        {group?.data?.map(color => (
+                            <h2 key={color.id}>{color.id} - {color.label}</h2>))}
+                    </Fragment>
+                )
+            })}
 
-            {/* <button onClick={() => setPageNumber(page => page - 1)} disabled={pageNumber === 1}>Prev Page</button>
-            <button onClick={() => setPageNumber(page => page + 1)} disabled={pageNumber === 4}>Next Page</button>  // 4 pages max */}
+            {/* {data?.data?.map(color => (
+                <div key={color.id}>{color.id} - {color.label}</div>
+            ))} */}
+
+            <button onClick={fetchNextPage} disabled={ !hasNextPage }>Load More</button>
+            {/* <button onClick={() => setPageNumber(page => page + 1)} disabled={pageNumber === 4}>Next Page</button>  // 4 pages max */}
         </div>)
 }
